7682cb097306fe3f812252746e2dd035043a3657
Ashley Zhuang <ashleyzhuang01@gmail.com>
2023-02-20 21:13:05 -0500

    Refactor client code

--- client.py
+++ client.py
@@ -1,9 +1,6 @@
 import time, socket
 from threading import Thread
 
-HOST = 'dhcp-10-250-203-22.harvard.edu'
-PORT = 1538
-
 MAX_REQUEST_LEN = 280
 
 OP_TO_OPCODE = {
@@ -15,76 +12,55 @@ OP_TO_OPCODE = {
     'exit': '6'
 }
 
-# Background thread listens for incoming messages to print
-def receive_msgs(s):
-    # Messages send as [status][is_chat][msg]
-    for msg in iter(lambda: s.recv(1024).decode(), ''):
-        status, is_chat, msg = ord(msg[0]), int(msg[1]), msg[2:]
+class Client:
+    def __init__(self, host='dhcp-10-250-203-22.harvard.edu', port=1538):
+        self.s = socket.socket()
+        self.host = host
+        self.port = port
 
-        # Message from another client
-        if is_chat:
-            msg = msg.split('|', 1)
-            sender = msg[0].strip()
-            msg = msg[1].strip()
+    def print_intro_message(self):
+        print(' -------------------------------------------------------------------------------------------------------------------')
+        print('|                                             Welcome to the Chat Room!                                             |')
+        print(' -------------------------------------------------------------------------------------------------------------------')
 
-            print(f'\nMessage from {sender}: {msg}\n')
-        # Message from the server
-        else:
-            if status == 0:
-                print(f'\nSUCCESS: {msg}\n')
-            elif status == 1:
-                print(f'\nERROR: {msg}\n')
-            elif status == 2:
-                print(f'\nSERVER MESSAGE: {msg}\n')
+        print('INSTRUCTIONS: This chat room supports the following requests in the format \'[request type]|[params]\'')
+        print('- create|[username] --> Create account')
+        print('- login|[username] --> Log into existing account')
+        print('- send|[recipient]|[message] --> Send a message to another user')
+        print('- list|[wildcard (optional)] --> List accounts, via Unix shell-style wildcard (no wildcard = all accounts listed)')
+        print('- delete --> Delete account')
+        print('- exit --> Exit the chat application')
 
-# TODO: fix intro message
+        print(' --------------------------------------------------------------------------------------------------------------------')
+    
+    # For background thread that listens for incoming messages to print
+    def receive_msgs(self, s):
+        # Messages send as [status][is_chat][msg]
+        for msg in iter(lambda: s.recv(1024).decode(), ''):
+            status, is_chat, msg = ord(msg[0]), int(msg[1]), msg[2:]
 
-# def print_intro_message():
-#     print(' -------------------------------------------------------------------------------------------------------------------')
-#     print('|                                             Welcome to the Chat Room!                                             |')
-#     print(' -------------------------------------------------------------------------------------------------------------------')
+            # Message from another client
+            if is_chat:
+                msg = msg.split('|', 1)
+                sender = msg[0].strip()
+                msg = msg[1].strip()
 
-#     print('INSTRUCTIONS: To send a message to a user, please input your message followed by \'>\' and the recipient\'s username.\n')
-#     print('For example:')
-#     print('Hello there, Bob! > bobs_username_123\n')
+                print(f'\nMessage from {sender}: {msg}\n')
+            # Message from the server
+            else:
+                if status == 0:
+                    print(f'\nSUCCESS: {msg}\n')
+                elif status == 1:
+                    print(f'\nERROR: {msg}\n')
+                elif status == 2:
+                    print(f'\nSERVER MESSAGE: {msg}\n')
 
-#     print('Messages will appear with the sender\'s username in front. For example: ')
-#     print('bob: Long time no see, Alice!\n')
-
-#     print('Enter LIST to list all accounts')
-#     print('Enter DELETE to delete your account')
-
-#     print('ENTER [e] TO EXIT\n')
-
-#     print(' -------------------------------------------------------------------------------------------------------------------')
-
-
-def main():
-    print('\nWelcome to Chat Room\n')
-    print('Initialising....\n')
-    time.sleep(1)
-
-    s = socket.socket()
-
-    print(f'\nTrying to connect to {HOST} ({PORT})\n')
-    time.sleep(1)
-    s.connect((HOST, PORT))
-    print('Connected...\n')
-
-    background_thread = Thread(target=receive_msgs, args=(s,))
-    background_thread.daemon = True
-    background_thread.start()
-
-    # print_intro_message()
-
-    # Send message to the server to deliver to recipient
-    while True:
-        request = input()
-
-        # Limit request length
+    # Validate requests before sending to server
+    def validate_request(self, request):
+        # Length limit
         if len(request) > MAX_REQUEST_LEN:
             print('\nINPUT ERROR: Please limit requests to 280 characters.\n')
-            continue
+            return 1
 
         op, msg = request.split('|', 1) if '|' in request else (request, '')
         op, msg = op.strip(), msg.strip()
@@ -92,31 +68,68 @@ def main():
         # Operation does not exist
         if op not in OP_TO_OPCODE:
             print('\nINPUT ERROR: Invalid operation. Please input your request as [operation]|[params].\n')
-            continue
+            return 1
 
         # Validate parameters
         if op == 'create' or op == 'login':
             if not msg:
                 print('\nINPUT ERROR: Unable to create account: Username cannot be blank.\n')
-                continue
+                return 1
         elif op == 'send':
             msg_params = msg.split('|', 1)
             if len(msg_params) < 2:
                 print('\nINPUT ERROR: Not enough parameters specified. To send a message to another user, please type 3|[recipient]|[message].\n')
-                continue
+                return 1
             if msg_params[1].strip() == '':
                 print('\nINPUT ERROR: Message cannot be blank.\n')
-                continue
+                return 1
 
+        return 0
+
+    # Encode request to match wire protocol and send to server
+    def pack_and_send_request(self, request):
+        op, msg = request.split('|', 1) if '|' in request else (request, '')
+        op, msg = op.strip(), msg.strip()
 
         opcode = OP_TO_OPCODE[op]
         encoded_request = (opcode + '|' + msg).encode() # TODO: send with header?
-        s.send(encoded_request) 
+        self.s.send(encoded_request) 
 
-        # Client is exiting 
-        if op == 'exit':
-            break
+    # Main execution for communicating with chat server
+    def connect_to_server(self):
+        print('\nWelcome to Chat Room\n')
+        print('Initialising....\n')
+        time.sleep(1)
 
+        print(f'\nTrying to connect to {self.host} ({self.port})\n')
+        time.sleep(1)
+        self.s.connect((self.host, self.port))
+        print('Connected...\n')
+
+        background_thread = Thread(target=self.receive_msgs, args=(self.s,))
+        background_thread.daemon = True
+        background_thread.start()
+
+        self.print_intro_message()
+
+        # Send message to the server to deliver to recipient
+        while True:
+            request = input()
+
+            status = self.validate_request(request)
+
+            # Continue if request is invalid
+            if status == 1:
+                continue
+
+            # Send request to server
+            self.pack_and_send_request(request)
+
+            # Client is exiting 
+            if request.split('|', 1)[0] == 'exit':
+                break
+            
 
 if __name__ == "__main__":
-    main()
+    client = Client()
+    client.connect_to_server()
